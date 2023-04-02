5c5d1aeddc7039067a55cbfc7c75f35a6d62b44f
samsonahh <swu30@stuy.edu>
2023-04-02 19:33:33 -0400

    Init

---
+++ src/App.js
@@ -0,0 +1,29 @@
+function Title(){
+  return(
+    <>
+    <h1>React One Pager</h1>
+    <h4>Hui Wang and Samson Wu</h4>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+    <Title />
+
+    <h3>React is a Javascript library for developing front end UI.</h3>
+    <li>React is useful because it only changes what needs to be changed. This makes development a lot faster.</li>
+
+    <h2>Getting React setup:</h2>
+    <li>Install Node.js using: sudo apt install nodejs</li>
+    <li>Create your React app in your directory using: npm create-react-app [PATH]</li>
+    <li>Start building your app by editing src/App.js</li>
+
+    <h2>React Component:</h2>
+
+    </>
+  );
+}
+
+export default App;
